4b149933acff80d11000c41af19ab5f5aea53391
Luke Videckis <luke.videckis@mines.sdsmt.edu>
2020-06-01 03:54:27 -0600

    rows,cols,bombs input is the same as the last input

--- app/src/main/java/com/example/minesweeper20/activity/StartScreenActivity.java
+++ app/src/main/java/com/example/minesweeper20/activity/StartScreenActivity.java
@@ -1,7 +1,9 @@
 package com.example.minesweeper20.activity;
 
 import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.InputFilter;
 import android.view.View;
@@ -22,7 +24,6 @@ import com.example.minesweeper20.miscHelpers.Test;
 //TODO: change game board scale to be pivoted around focus point instead of the middle of the screen
 //TODO: undo button - only when you lose
 //TODO: add settings page were you can choose whether or not to have a zero-start, also choose iteration limit of backtracking solver, also choose defaults for Flag Mode, Game mode, etc
-//TODO: make rows/cols/bombs be the same as the last played game
 //TODO: make sure back button on bottom nav bar works
 //TODO: when you lose, you can't click the back button, you have to click the okay popup button - fix this
 //TODO: Backtracking popup shows twice when clicking bomb probability
@@ -34,8 +35,14 @@ import com.example.minesweeper20.miscHelpers.Test;
 
 public class StartScreenActivity extends AppCompatActivity implements View.OnClickListener {
 
+	public static final String
+			MY_PREFERENCES = "MyPrefs",
+			NUMBER_OF_ROWS = "numRows",
+			NUMBER_OF_COLS = "numCols",
+			NUMBER_OF_BOMBS = "numBombs";
 	private PopupWindow newGamePopup;
 	private PopupWindow gridTooSmallPopup;
+	private SharedPreferences sharedPreferences;
 
 	@Override
 	protected void onCreate(final Bundle savedInstanceState) {
@@ -44,6 +51,8 @@ public class StartScreenActivity extends AppCompatActivity implements View.OnCli
 
 		Button startGameButton = findViewById(R.id.startNewGame);
 		startGameButton.setOnClickListener(this);
+
+		sharedPreferences = getSharedPreferences(MY_PREFERENCES, Context.MODE_PRIVATE);
 
 		setUpNewGamePopup();
 		setUpGridTooSmallPopup();
@@ -66,13 +75,19 @@ public class StartScreenActivity extends AppCompatActivity implements View.OnCli
 			@SuppressLint("InflateParams")
 			@Override
 			public void onClick(View view) {
-				EditText rowInput = newGamePopup.getContentView().findViewById(R.id.rowsInput);
-				EditText colInput = newGamePopup.getContentView().findViewById(R.id.colsInput);
-				EditText bombInput = newGamePopup.getContentView().findViewById(R.id.bombsInput);
+				final EditText rowInput = newGamePopup.getContentView().findViewById(R.id.rowsInput);
+				final EditText colInput = newGamePopup.getContentView().findViewById(R.id.colsInput);
+				final EditText bombInput = newGamePopup.getContentView().findViewById(R.id.bombsInput);
 
 				final int numberOfRows = getNumberInput(rowInput.getText().toString());
 				final int numberOfCols = getNumberInput(colInput.getText().toString());
 				final int numberOfBombs = getNumberInput(bombInput.getText().toString());
+
+				SharedPreferences.Editor editor = sharedPreferences.edit();
+				editor.putInt(NUMBER_OF_ROWS, numberOfRows);
+				editor.putInt(NUMBER_OF_COLS, numberOfCols);
+				editor.putInt(NUMBER_OF_BOMBS, numberOfBombs);
+				editor.apply();
 
 				if (MinesweeperGame.tooManyBombsForZeroStart(numberOfRows, numberOfCols, numberOfBombs)) {
 					LinearLayout startScreen = findViewById(R.id.start_screen);
@@ -96,6 +111,15 @@ public class StartScreenActivity extends AppCompatActivity implements View.OnCli
 		final EditText rowsInput = newGamePopup.getContentView().findViewById(R.id.rowsInput);
 		final EditText colsInput = newGamePopup.getContentView().findViewById(R.id.colsInput);
 		final EditText bombsInput = newGamePopup.getContentView().findViewById(R.id.bombsInput);
+
+		final int previousRows = sharedPreferences.getInt(NUMBER_OF_ROWS, 5);
+		final int previousCols = sharedPreferences.getInt(NUMBER_OF_COLS, 5);
+		final int previousBombs = sharedPreferences.getInt(NUMBER_OF_BOMBS, 5);
+
+		rowsInput.setText(String.valueOf(previousRows));
+		colsInput.setText(String.valueOf(previousCols));
+		bombsInput.setText(String.valueOf(previousBombs));
+
 
 		NumberInputFilterMinMax rowsAndColsFilter = new NumberInputFilterMinMax(1, 50);
 		rowsInput.setFilters(new InputFilter[]{rowsAndColsFilter});
